9ba122ca6665377436dc4d59751716187d26b346
Pashckevich227 <iport234@gmail.com>
2022-10-25 11:59:54 +0300

    Create: get one position, error method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response, request
 from models import Book
 from crud import Session
 
@@ -6,12 +6,32 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-# GET METHODS
+# GET ALL METHODS
 @app.route("/", methods=['GET'])
 def get_books():
     s = Session()
     books = s.query(Book).order_by(Book.id).all()
     return jsonify({'books:': list(el.json() for el in books)})
+
+
+# GET ONE POSITION
+@app.route("/<int:book_id>", methods=['GET'])
+def get_book(book_id):
+    s = Session()
+    book = list(el.json() for el in s.query(Book).all())
+    book = list(filter(lambda x: x['id'] == book_id, book))
+    if len(book) == 0:
+        abort(404)
+    return jsonify({'books:': book[0]})
+
+
+# ERROR METHODS
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)  # 404 - Not found
+
+
+
 
 
 if __name__ == '__main__':
